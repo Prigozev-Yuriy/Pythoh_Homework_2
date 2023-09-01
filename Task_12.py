2fb9b08b4099c3a36b4d62df8f529e468fdb07c8
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-09-01 18:08:55 +0300

    task_120 commit

---
+++ Task_12.py
@@ -0,0 +1,13 @@
+# Задача 12:
+# Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике.
+# Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать.
+# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P.
+# Помогите Кате отгадать задуманные Петей числа.
+
+sum_of_numbers = int(input('Введите сумму чисел: '))
+product_of_numbers = int(input('Введите произведение чисел: '))
+result = []
+for i in range (sum_of_numbers + product_of_numbers):
+    if i == (sum_of_numbers * i - product_of_numbers) ** 0.5:
+        result.append(i)
+print(*result if len(result) == 2 else result + result)
